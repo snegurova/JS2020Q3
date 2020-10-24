593e796cb015f4128d785af129293c1cc8b902a7
snegurova <snegurova@gmail.com>
2020-10-24 21:54:28 +0300

    fix: add overflow to prevent page scroll

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,15 +1,19 @@
 const humburgerButton = document.querySelector('.humburger-button');
 const navbar = document.querySelector('.navbar');
 const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
 
 humburgerButton.addEventListener('click', () => {
   navbar.classList.toggle('active');
   navbar.classList.toggle('closed');
   overlay.classList.toggle('show');
+  body.style.overflow === '' ? body.style.overflow = 'hidden' : body.style.overflow = '';
+
 });
 overlay.addEventListener('click', () => {
   navbar.classList.toggle('active');
   navbar.classList.toggle('closed');
   overlay.classList.toggle('show');
+  body.classList.toggle('fixed');
 });
 
