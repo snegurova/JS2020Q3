1fa818c3b78b4d9aeabea14eb001a912f3d52f0f
snegurova <snegurova@gmail.com>
2020-11-15 14:28:59 +0200

    feat: draw game field

--- gem-puzzle/src/js/game.js
+++ gem-puzzle/src/js/game.js
@@ -8,18 +8,52 @@ export default class Game {
     this.ctx = ctx;
     this.clicks = 0;
     this.createCells(this.cellSize, this.cellCount);
+    this.clicksCount = 0;
   }
 
   createCells(cellSize, cellCount) {
-    for (let i = 0; i < cellCount; i++) {
-      let row = [];
-      for (let j = 0; j < cellCount; j++) {
-        const value = i * cellCount + j;
-        row.push(new Cell(value, i, j, cellSize));
-      }
-      this.cells.push(row);
+    for (let i = 0; i < cellCount * cellCount; i++) {
+      const x = i % cellCount;
+      const y = Math.trunc(i / cellCount);
+      this.cells.push(new Cell(
+        i === cellCount * cellCount - 1 ? 0 : i + 1,
+        x, y, cellSize));
     }
 
+  }
+
+  getClicks() {
+    return this.clicks;
+  }
+
+  drawCell(x, y) {
+    this.ctx.fillStyle = '#FFB93B';
+    this.ctx.fillRect(x + 1, y + 1, this.cellSize - 2, this.cellSize - 2);
+  }
+
+  drawNum(value, x, y) {
+    this.ctx.font = `bold ${this.cellSize / 2}px Sans`;
+    this.ctx.textAlign = 'center';
+    this.ctx.textBaseline = 'middle';
+    this.ctx.fillStyle = '#222';
+    this.ctx.fillText(value, x, y);
+  }
+
+  getEmptyCell() {
+    return this.cells.find(cell => cell.value === 0);
+  }
+
+  draw() {
+    this.cells.forEach(cell => {
+      if (cell.value > 0) {
+        this.drawCell(cell.x * this.cellSize, cell.y * this.cellSize);
+        this.drawNum(
+          cell.value,
+          cell.x * this.cellSize + this.cellSize / 2,
+          cell.y * this.cellSize + this.cellSize / 2
+        );
+      }
+    });
   }
 }
 
--- gem-puzzle/src/js/index.js
+++ gem-puzzle/src/js/index.js
@@ -1,5 +1,14 @@
 import Game from './game'
 
-const game = new Game(80, 4);
+const canvas = document.createElement('canvas');
+canvas.width = 320;
+canvas.height = 320;
+const ctx = canvas.getContext("2d");
+document.body.appendChild(canvas);
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+const game = new Game(80, 4, ctx);
+game.draw();
 console.log(game.cells);
+console.log(game.getEmptyCell());
 console.log('start');
